e3594cee6b5f702c4e1ce1de0f0d865bdd6d0bcb
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-09-11 18:58:30 +0100

    Refactor loyalty card cuts in db.json and update appointment history rendering

--- src/modules/form.js
+++ src/modules/form.js
@@ -1,3 +1,4 @@
+import { updateProgressBar } from './progress-bar.js';
 import { fetchAndSetUser, getUserState, getIsFetching } from './userState.js';
 
 const form = document.querySelector('form.input-container');
@@ -19,7 +20,11 @@ form.addEventListener('submit', async (e) => {
       const success = await fetchAndSetUser(inputId.value);
       if (success) {
         const user = getUserState();
+
         renderUser(user);
+        renderAppointmentHistory(user.appointmentHistory);
+        updateProgressBar(user.loyaltyCard.totalCuts, user.loyaltyCard.cutsNeeded);
+        
         main.classList.remove('hidden');
       } else {
         alert('User not found!');
@@ -47,4 +52,47 @@ function renderUser(user) {
 
   h2UserName.textContent = user.name;
   pMemberSince.textContent = `Member since: ${user.clientSince}`;
+}
+
+function renderAppointmentHistory(appointmentHistory) {
+  const spanCutsCompleted = document.querySelector('.history-header-container span');
+  spanCutsCompleted.textContent = `${appointmentHistory.length} cuts`;
+
+  const ulAppointmentHistory = document.querySelector('ul.history-container');
+  ulAppointmentHistory.innerHTML = '';
+
+  appointmentHistory.forEach(appointment => {
+    const li = document.createElement('li');
+    li.classList.add('history-item');
+    
+    const divHistoryDateTime = document.createElement('div');
+    divHistoryDateTime.classList.add('history-date-time');
+
+    const spanDate = document.createElement('span');
+    spanDate.classList.add('history-date');
+
+    const spanTime = document.createElement('span');
+    spanTime.classList.add('history-time');
+
+    const divPinCheckLightContainer = document.createElement('div');
+    divPinCheckLightContainer.classList.add('pin-check-light-container');
+
+    const imgPinCheckLight = document.createElement('img');
+    imgPinCheckLight.classList.add('history-checkmark');
+
+    spanDate.textContent = appointment.date;
+    spanTime.textContent = appointment.time;
+    imgPinCheckLight.src = '../public/images/PinCheckLight.png';
+    imgPinCheckLight.alt = 'Completed';
+
+    divHistoryDateTime.appendChild(spanDate);
+    divHistoryDateTime.appendChild(spanTime);
+
+    divPinCheckLightContainer.appendChild(imgPinCheckLight);
+
+    li.appendChild(divHistoryDateTime);
+    li.appendChild(divPinCheckLightContainer);
+
+    ulAppointmentHistory.appendChild(li);
+  });
 }
--- src/modules/progress-bar.js
+++ src/modules/progress-bar.js
@@ -1,6 +1,4 @@
-const CUTS_TRESHOLD = 10;
-
-export function updateProgressBar(cuts) {
+export function updateProgressBar(cuts, CUTS_TRESHOLD = 10) {
   if (cuts < 0 || cuts > CUTS_TRESHOLD) {
     alert('Invalid number of cuts');
     return;
@@ -8,8 +6,13 @@ export function updateProgressBar(cuts) {
 
   const progressBar = document.querySelector('.progress-fill');
   const progressText = document.querySelector('.progress-bar span');
+  const strongCutsRemaining = document.querySelector('.cuts-remaining strong');
 
   const progress = cuts / CUTS_TRESHOLD * 100;
   progressBar.style.width = `${progress}%`;
   progressText.textContent = `${cuts} of ${CUTS_TRESHOLD}`;
+  strongCutsRemaining.textContent = CUTS_TRESHOLD - cuts;
+
+  console.log('cuts', cuts);
+  console.log('progress', progress);
 }
